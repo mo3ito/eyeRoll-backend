ee42a5d5e897152de7284d4ec3baa5a671e5b8dc
mo3ito <mostafa.tar90@gmail.com>
2023-10-24 17:31:49 +0330

    add validation hour & minuts for get roll

--- controllers/bussinessOwnerControllers/rolls.js
+++ controllers/bussinessOwnerControllers/rolls.js
@@ -92,59 +92,120 @@ const getAllAlgoritm = async (req, res) => {
 };
 
 
+const getAllRollsList = async (req , res) =>{
 
-const discountInformation = async (req, res) => {
+  const allRolls = await RollOptionModel.find({})
+  res.status(200).json(allRolls)
+
+}
+
+
+// const getRoll = async (req, res) => {
+//   const { user_id, businessOwner_id } = req.body;
+
+//   const user = await UsersModel.findOne({_id : user_id });
+//   const businessOwner = await BusinessOwnersModel.findById(businessOwner_id);
+
+//   // if (!user) {
+//   //   res.status(400).json({
+//   //     message: "user not found"
+//   //   });
+//   // } 
+
+//   // if (!businessOwner) {
+//   //   return res.status(400).json({
+//   //     message: "business owner not found"
+//   //   });
+//   // }
+
+//   const rollOptionBusinessOwner = await RollOptionModel.findOne({businessOwner_id})
+
+//   const currentDate = new Date();
+
+//    let selectedData;
+
+//   // اگر تاریخ فعلی در دوره "پیک" باشد و دوره "پیک" تعریف شده باشد
+//   if (
+//     rollOptionBusinessOwner.first_date_peak &&
+//     rollOptionBusinessOwner.last_date_peak &&
+//     currentDate >= new Date(rollOptionBusinessOwner.first_date_peak) &&
+//     currentDate <= new Date(rollOptionBusinessOwner.last_date_peak)
+//   ) {
+//     // اگر تاریخ فعلی در بازه "پیک" باشد
+//     selectedData = {
+//       min_percentage: rollOptionBusinessOwner.min_percentage_peak,
+//       max_percentage: rollOptionBusinessOwner.max_percentage_peak
+//     };
+//   } else if (
+//     currentDate >= new Date(rollOptionBusinessOwner.first_date) &&
+//     currentDate <= new Date(rollOptionBusinessOwner.last_date)
+//   ) {
+//     // اگر تاریخ فعلی در دوره "عادی" باشد و تاریخ‌های مشترک نباشند
+//     selectedData = {
+//       min_percentage: rollOptionBusinessOwner.minـpercentage,
+//       max_percentage: rollOptionBusinessOwner.maxـpercentage
+//     };
+//   }
+
+//   if (selectedData) {
+//     res.status(200).json(selectedData);
+//   } else {
+//     // اگر تاریخ فعلی بین هیچ یک از دوره‌ها نبود
+//     res.status(400).json({
+//       message: "No applicable discount information for the current date"
+//     });
+//   }
+// };
+
+const getRoll = async (req, res) => {
   const { user_id, businessOwner_id } = req.body;
 
-  const user = await UsersModel.findOne({_id : user_id });
+  const user = await UsersModel.findOne({ _id: user_id });
   const businessOwner = await BusinessOwnersModel.findById(businessOwner_id);
 
-  // if (!user) {
-  //   res.status(400).json({
-  //     message: "user not found"
-  //   });
-  // } 
 
-  // if (!businessOwner) {
-  //   return res.status(400).json({
-  //     message: "business owner not found"
-  //   });
-  // }
-
-  const rollOptionBusinessOwner = await RollOptionModel.findOne({businessOwner_id})
-
-  const currentDate = new Date();
+  const rollOptionBusinessOwner = await RollOptionModel.findOne({ businessOwner_id });
 
   let selectedData;
 
+  const currentDate = new Date();
+
+  const peakStartTime = new Date(rollOptionBusinessOwner.first_date_peak);
+  const peakEndTime = new Date(rollOptionBusinessOwner.last_date_peak);
+  const normalStartTime = new Date(rollOptionBusinessOwner.first_date);
+  const normalEndTime = new Date(rollOptionBusinessOwner.last_date);
+
   if (
-    currentDate >= new Date(rollOptionBusinessOwner.first_date) &&
-    currentDate <= new Date(rollOptionBusinessOwner.last_date)
+    (currentDate >= peakStartTime && currentDate <= peakEndTime) &&
+    (peakStartTime.getHours() == normalStartTime.getHours() && peakStartTime.getMinutes() == normalStartTime.getMinutes()) &&
+    (peakEndTime.getHours() == normalEndTime.getHours() && peakEndTime.getMinutes() == normalEndTime.getMinutes())
   ) {
+    // اگر تاریخ فعلی در تایم پیک و تایم عادی با هم مشترک باشد
     selectedData = {
-      min_percentage: rollOptionBusinessOwner.minـpercentage,
-      max_percentage: rollOptionBusinessOwner.maxـpercentage
+      min_percentage: rollOptionBusinessOwner.min_percentage_peak,
+      max_percentage: rollOptionBusinessOwner.max_percentage_peak
     };
-  } else if (
-    currentDate >= new Date(rollOptionBusinessOwner.first_date_peak) &&
-    currentDate <= new Date(rollOptionBusinessOwner.last_date_peak)
-  ) {
+  } else if (currentDate >= peakStartTime && currentDate <= peakEndTime) {
+    // اگر تاریخ فعلی در تایم پیک باشد
     selectedData = {
       min_percentage: rollOptionBusinessOwner.min_percentage_peak,
       max_percentage: rollOptionBusinessOwner.max_percentage_peak
     };
   } else {
-    // اگر تاریخ فعلی بین هیچ یک از دو دوره نبود
-    return res.status(400).json({
+    // در سایر موارد
+    selectedData = {
+      min_percentage: rollOptionBusinessOwner.minـpercentage,
+      max_percentage: rollOptionBusinessOwner.maxـpercentage
+    };
+  }
+
+  if (selectedData) {
+    res.status(200).json(selectedData);
+  } else {
+    res.status(400).json({
       message: "No applicable discount information for the current date"
     });
   }
+};
 
-  res.status(200).json(selectedData);
-
-  
- 
-  
-}
-
-module.exports = { getAllAlgoritm , discountInformation};
+module.exports = { getAllAlgoritm , getRoll , getAllRollsList};
--- routes/businessOwners/rolls.js
+++ routes/businessOwners/rolls.js
@@ -1,13 +1,13 @@
 const express = require("express")
 const router = express.Router()
-const {getAllAlgoritm , discountInformation } = require("../../controllers/bussinessOwnerControllers/rolls")
+const {getAllAlgoritm , getRoll , getAllRollsList } = require("../../controllers/bussinessOwnerControllers/rolls")
 
 
 
 
-
-router.post("/roll/getAll", getAllAlgoritm)
-router.post("/roll/discount-information" , discountInformation )
+router.get("/roll/get-all-rolls-list",getAllRollsList)
+router.post("/roll/get-roll-algoritm", getAllAlgoritm)
+router.post("/roll/get-roll" , getRoll )
 
 
 
