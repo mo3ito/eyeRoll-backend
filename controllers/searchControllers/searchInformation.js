43899ed3243007bb96125cf0824f54f4b88c4743
mo3ito <mostafa.tar90@gmail.com>
2023-12-15 17:17:09 +0330

    add getBusinessOwnerInfosSearched

--- controllers/searchControllers/searchInformation.js
+++ controllers/searchControllers/searchInformation.js
@@ -16,6 +16,29 @@ const searchInformation = async (req, res) => {
     }
 }
 
+const getBusinessOwnerInfosSearched = async (req , res)=>{
+
+    const businessOwnerId = req.query.businessOwnerId
+
+    try {
+
+        if(!businessOwnerId){
+            return res.status(400).json({
+                message: "businessOwner id not found"
+            })
+        }
+
+       const infos = await BusinessOwnerModel.findById(businessOwnerId).select('_id name last_name country_name state_name city_name address brand_name work_phone')
+       console.log(infos);
+      return res.status(200).json(infos)
+        
+    } catch (error) {
+        console.error(error)
+            res.status(500).json(error.message)
+    }
+
+}
 
 
-module.exports = {searchInformation};
+
+module.exports = {searchInformation , getBusinessOwnerInfosSearched};
--- routes/searchInformation/searchInformation.js
+++ routes/searchInformation/searchInformation.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const router = express.Router()
-const {searchInformation} = require("../../controllers/searchControllers/searchInformation")
+const {searchInformation , getBusinessOwnerInfosSearched} = require("../../controllers/searchControllers/searchInformation")
 
 router.get("/search-information" , searchInformation)
+router.get("/get-businessOwner-infos-searched" , getBusinessOwnerInfosSearched)
 
 
 
